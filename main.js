4687a901ea305585fb0b74dfe77eb10d60724f86
lkm779 <luiskm7796@gmail.com>
2022-03-23 00:10:11 +0000

    new updates

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ css-----------------------------------
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "09/30/",
+      dayMonth = "04/06/",
       birthday = dayMonth + yyyy;
   
   today = mm + "/" + dd + "/" + yyyy;
